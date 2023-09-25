73688f05038c9d9a422c7b251fc4a950df127ddf
Miguel Angel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2023-09-25 14:58:51 -0600

    Jest Configuration

---
+++ api/jest.config.ts
@@ -0,0 +1,8 @@
+// jest.config.js or jest.config.ts
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/*.test.ts'], // Adjust the pattern to match your test files
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+  
