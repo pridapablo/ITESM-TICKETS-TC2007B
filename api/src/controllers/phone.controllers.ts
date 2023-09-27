f6bc9600ec70f8ce5d8c992ec012c99918a5c525
Pablo Banzo <pridapablo@gmail.com>
2023-09-26 23:48:52 -0600

    chatbot working

--- api/src/controllers/phone.controllers.ts
+++ api/src/controllers/phone.controllers.ts
@@ -55,11 +55,8 @@ export const handleTicket = async (req, res) => {
                 case 1:
                     //Store ticket description directly
                     u.chat_ticket_description = Body;
-                    u.chat_state = 2;
                     await u.save();
-                    break;
 
-                case 2:
                      // Send categories menu
                     const categoriesMenuText = menu.join(', ');
                     await client.messages.create({
@@ -67,25 +64,27 @@ export const handleTicket = async (req, res) => {
                         from: "whatsapp:+14155238886",
                         to: `whatsapp:+${WaId}`,
                     });
-                    u.chat_state = 3;
+                    u.chat_state = 2;
                     await u.save();
                     break;
             
-                case 3:
+                case 2:
                     // Store category
                     const index = parseInt(Body) - 1; // Subtract 1 because array indices start at 0
                     if (index >= 0 && index < menu.length) {
                         const processedBody = menu[index];
                         u.chat_ticket_category = processedBody;
-                        u.chat_state = 4;
                         await u.save();
                     }
                     else {
                         // Handle the case where the index is out of bounds
+                        await client.messages.create({
+                            body: "Por favor, selecciona una opción válida. (Digita el número de la opción)",
+                            from: "whatsapp:+14155238886",
+                            to: `whatsapp:+${WaId}`,
+                        });
                     }
-                    break;
-                
-                case 4:
+
                     // Send subcategories menu based on category
                     if (u.chat_ticket_category && u.chat_ticket_category in subMenu) {
                         console.log("entro", u.chat_ticket_category);
@@ -95,15 +94,20 @@ export const handleTicket = async (req, res) => {
                             from: "whatsapp:+14155238886",
                             to: `whatsapp:+${WaId}`,
                         });
-                        u.chat_state = 5;
+                        u.chat_state = 3;
                         await u.save();
                     } else {
                         console.log("no entro", u.chat_ticket_category);
                         // Handle the case where the category is not in the subMenu
+                        await client.messages.create({
+                            body: "Por favor, selecciona una opción válida. (Digita el número de la opción)",
+                            from: "whatsapp:+14155238886",
+                            to: `whatsapp:+${WaId}`,
+                        });
                     }
                     break;
                 
-                case 5:
+                case 3:
                     // Store subcategory
                     // Assuming Body contains the integer response
                     const index2 = parseInt(Body) - 1; // Subtract 1 because array indices start at 0
@@ -114,73 +118,80 @@ export const handleTicket = async (req, res) => {
                         if (index2 >= 0 && index2 < subcategories.length) {
                             const processedBody2 = subcategories[index2];
                             u.chat_ticket_subcategory = processedBody2;
-                            u.chat_state = 6;
                             await u.save();
                         } else {
                             // Handle the case where the index is out of bounds
+                            await client.messages.create({
+                                body: "Por favor, selecciona una opción válida. (Digita el número de la opción)",
+                                from: "whatsapp:+14155238886",
+                                to: `whatsapp:+${WaId}`,
+                            });
                         }
                     } else {
                         // Handle the case where the category is not in the subMenu
+                        await client.messages.create({
+                            body: "Por favor, selecciona una opción válida. (Digita el número de la opción)",
+                            from: "whatsapp:+14155238886",
+                            to: `whatsapp:+${WaId}`,
+                        });
                     }
-                    break;
-                
-                case 6:
+
+                    // Send priorities menu
                     const prioritiesMenuText = ["1", "2", "3", "4", "5"].join(', ');
                     await client.messages.create({
                         body: `Por favor, selecciona una prioridad. Aquí están las opciones: ${prioritiesMenuText}`,
                         from: "whatsapp:+14155238886",
                         to: `whatsapp:+${WaId}`,
                     });
-                    u.chat_state = 7;
+                    u.chat_state = 4;
                     await u.save();
                     break;
                 
-                case 7:
+                case 4:
                     // Store priority
                     const priority = parseInt(Body);
                     if (isNaN(priority) || priority < 1 || priority > 5) {
-                        // Handle the case where the input is not a number or not within the range
+                        // Handle the case where the priority is not a number or is out of bounds
+                        await client.messages.create({
+                            body: "Por favor, selecciona una opción válida. (Digita la prioridad en número del 1 al 5)",
+                            from: "whatsapp:+14155238886",
+                            to: `whatsapp:+${WaId}`,
+                        });
                     } else {
                         const processedBody3 = priority;
                         u.chat_ticket_priority = processedBody3;
-                        u.chat_state = 8;
                         await u.save();
                     }
-                    break;
-                
-                case 8:
-                    // Ask for ticket confirmation
+
+                     // Ask for ticket confirmation
                     await client.messages.create({
-                        body: "Por favor, confirma tu ticket.",
-                        from: "whatsapp:+14155238886",
-                        to: `whatsapp:+${WaId}`,
-                    });
-                    u.chat_state = 9;
-                    await u.save();
-                    break;
-                
-               case 9:
-                // Print ticket details
-                await client.messages.create({
-                    body: `Aquí están los detalles de tu ticket:\n\n
-                    Descripción: ${u.chat_ticket_description}
-                    Categoría: ${u.chat_ticket_category}
-                    Subcategoría: ${u.chat_ticket_subcategory}
-                    Prioridad: ${u.chat_ticket_priority} \n\n
-                    Por favor confirma si los detalles son correctos. Responde con "si" o "no".`,
+                    body: `Aquí están los detalles de tu ticket:\n\nDescripción: ${u.chat_ticket_description}\nCategoría: ${u.chat_ticket_category}\nSubcategoría: ${u.chat_ticket_subcategory}\nPrioridad: ${u.chat_ticket_priority}\n\nPor favor confirma si los detalles son correctos. Responde con "si" o "no".`,
                     from: "whatsapp:+14155238886",
                     to: `whatsapp:+${WaId}`,
-                });
-                u.chat_state = 10;
-                await u.save();
-                break;
-                
-                case 10:
+                    });
+                    u.chat_state = 5;
+                    await u.save();
+                    break;
+              
+                case 5:
                     // Validate yes/no
                     const processedBody4 = Body.toLowerCase();
 
                     if (processedBody4 === 'si') {
-                        u.chat_state = 11;
+                         const t = new ticket({
+                        description: u.chat_ticket_description,
+                        category: u.chat_ticket_category,
+                        subcategory: u.chat_ticket_subcategory,
+                        priority: u.chat_ticket_priority,
+                        user: u._id,
+                        });
+                        await t.save();
+                        await client.messages.create({
+                            body: "Ticket creado con éxito.",
+                            from: "whatsapp:+14155238886",
+                            to: `whatsapp:+${WaId}`,
+                        });
+                        u.chat_state = 0;
                         await u.save();
                     } else if (processedBody4 === 'no') {
                         u.chat_state = 0;
@@ -189,34 +200,15 @@ export const handleTicket = async (req, res) => {
                             from: "whatsapp:+14155238886",
                             to: `whatsapp:+${WaId}`,
                         });
-
                         await u.save();
                     } else {
-                        // Handle the case where the input is not yes/no
+                        await client.messages.create({
+                            body: "Por favor, responde con 'si' o 'no'.",
+                            from: "whatsapp:+14155238886",
+                            to: `whatsapp:+${WaId}`,
+                        });
                     }
-                    u.chat_state = 11;
-                    await u.save();
                     break;
-                
-                case 11:
-                    // Create ticket
-                    const t = new ticket({
-                        description: u.chat_ticket_description,
-                        category: u.chat_ticket_category,
-                        subcategory: u.chat_ticket_subcategory,
-                        priority: u.chat_ticket_priority,
-                        user: u._id,
-                    });
-                    await t.save();
-                    await client.messages.create({
-                        body: "Ticket creado con éxito.",
-                        from: "whatsapp:+14155238886",
-                        to: `whatsapp:+${WaId}`,
-                    });
-                    u.chat_state = 0;
-                    await u.save();
-                    break;
-
                 default:
                     await client.messages.create({
                         body: "Estado desconocido, reiniciando chatbot.",
@@ -227,7 +219,6 @@ export const handleTicket = async (req, res) => {
                     await u.save();
                     break;
             }
-               
         }
     } catch (e) {
         await client.messages.create({
