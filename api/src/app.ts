a2faf8af869f1bfb0caa05df5541da84d647f887
Miguel Angel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2023-09-29 00:06:11 -0600

    Cors install

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import  morgan from 'morgan';
+import cors from 'cors'
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
+app.use(cors());
+
 
 //import routes
 import pruebaRoute from './routes/prueba.routes'
