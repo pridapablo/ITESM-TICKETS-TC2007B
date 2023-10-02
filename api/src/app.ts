7171c2b2d9d2bee4b35ce54d31b6a6c29d530a6b
beborico1 <beborico16@hotmail.com>
2023-10-02 17:15:44 -0600

    cambios del frontend

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,18 +1,20 @@
 import express from "express";
-import  morgan from 'morgan';
+import morgan from 'morgan';
+import cors from 'cors';
 
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 //import routes
 import pruebaRoute from './routes/prueba.routes'
 import usersRoute from './routes/user.routes'
 import ticketRoute from './routes/tickets.routes'
-import phoneRoute from './routes/phone.routes'
+// import phoneRoute from './routes/phone.routes'
 
 
 
@@ -20,7 +22,7 @@ import phoneRoute from './routes/phone.routes'
 app.use('/', pruebaRoute);
 app.use('/user', usersRoute);
 app.use('/ticket', ticketRoute);
-app.use('/phone', phoneRoute);
+// app.use('/phone', phoneRoute);
 
 
 export default app;
--- frontend/src/data/dataProvider.ts
+++ frontend/src/data/dataProvider.ts
@@ -1,5 +1,22 @@
 import jsonServerProvider from "ra-data-json-server";
+import { GetListParams } from 'react-admin';
 
-export const dataProvider = jsonServerProvider(
+const originalDataProvider = jsonServerProvider(
   import.meta.env.VITE_JSON_SERVER_URL
 );
+
+const customDataProvider = {
+  ...originalDataProvider,
+  getList: (resource: string, params: GetListParams) => {
+    if (resource === 'tickets') {
+      return Promise.resolve({
+        data: [],
+        total: 0
+      });
+    }
+    return originalDataProvider.getList(resource, params);
+  },
+  // Puedes hacer lo mismo para otros métodos como getOne, getMany, etc. si es necesario
+};
+
+export const dataProvider = customDataProvider;
