794a0353f8218309c7833a3c618aea3713a33d38
Pablo Banzo <pridapablo@gmail.com>
2023-10-04 17:30:29 -0600

    new data provider

--- frontend/src/data/dataProvider.ts
+++ frontend/src/data/dataProvider.ts
@@ -1,7 +1,22 @@
 import jsonServerProvider from "ra-data-json-server";
+import { fetchUtils } from 'react-admin';
+
+interface FetchOptions {
+  // eslint-disable-next-line no-undef
+  headers?: HeadersInit;
+}
+
+const fetchJsonUtil = (url: string, options: FetchOptions = {}) => {
+  options.headers = {
+    ...options.headers,
+    "Authentication": localStorage.getItem("auth") || "",
+  };
+  return fetchUtils.fetchJson(url, options);
+};
 
 const originalDataProvider = jsonServerProvider(
-  import.meta.env.VITE_JSON_SERVER_URL
+import.meta.env.VITE_JSON_SERVER_URL,
+fetchJsonUtil
 );
 
 export const dataProvider = originalDataProvider;
