bd32f3c3ee698f145efc989829fe5f237423aec3
beborico1 <beborico16@hotmail.com>
2023-10-06 11:14:31 -0700

    Popup para el numero de telefono

--- frontend/src/layout/TopBar.tsx
+++ frontend/src/layout/TopBar.tsx
@@ -1,37 +1,34 @@
-import {
-  AppBar,
-} from "react-admin";
-import { styled } from "@mui/system";
+import React from 'react';
+import { AppBar, AppBarProps } from 'react-admin';
+import { styled } from '@mui/system';
 import logo from '../assets/logo.png';
 import { Link } from "react-router-dom";
+import CustomUserMenu from "../auth/CustomUserMenu";
+
+const LogoContainer = styled('div')(({ theme }) => ({
+  flex: 1,
+  display: 'flex',
+  justifyContent: 'flex-start',
+  paddingLeft: theme.spacing(2),
+  paddingRight: theme.spacing(2),
+  marginTop: theme.spacing(1),
+  marginBottom: theme.spacing(2),
+}));
 
 const StyledAppBar = styled(AppBar)(({ theme }) => ({
-  '&.MuiAppBar-root': {
-    display: 'flex',
-    minHeight: '64px',
-    justififyContent: 'center',
-    backgroundColor: theme.palette.mode === 'light' ? '#4CAF50' : '#006847', // verde claro para el tema claro y verde oscuro para el tema oscuro
-  },
-  '& .logoContainer': {
-    flex: 1,
-    display: 'flex',
-    justifyContent: 'flex-start',
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(2),
-  },
-})) as typeof AppBar;
+  backgroundColor: theme.palette.mode === 'light' ? '#4CAF50' : '#006847', // verde claro para el tema claro y verde oscuro para el tema oscuro
+}));
 
-export const TopBar = () => {
+const TopBar = (props: AppBarProps) => {
   return (
-    <StyledAppBar position="fixed">
-      <div className="logoContainer">
+    <StyledAppBar {...props} userMenu={<CustomUserMenu />}>
+      <LogoContainer>
         <Link to="/">
           <img src={logo} alt="Logo" height="60" />
         </Link>
-      </div>
+      </LogoContainer>
     </StyledAppBar>
   );
-};
+}
 
+export default TopBar;
