9db565404c5161fbe6757253deaf7d23afabce84
Pablo Banzo <pridapablo@gmail.com>
2023-10-09 18:51:48 -0600

    working 4/5 categories

--- api/src/controllers/report.controller.ts
+++ api/src/controllers/report.controller.ts
@@ -94,13 +94,104 @@ const calculateTicketCounts = async () => {
     }
 };
 
+const calculateMostReportedCategories = async () => {
+    try {
+        const aggregationPipeline = [
+            {
+                $group: {
+                    _id: "$classification",
+                    count: { $sum: 1 }
+                }
+            },
+            { $sort: { count: -1 } }  // Sort categories in descending order of count
+        ];
+
+        const result = await ticket.aggregate(aggregationPipeline as any).exec();
+        return result;
+
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error calculating most reported categories');
+    }
+};
+
+const calculateTicketStats = async () => {
+    try {
+        // Current date and start of the week (7 days ago)
+        const now = new Date();
+        const startOfWeek = new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+        const aggregationPipeline = [
+            {
+                $lookup: {
+                    from: "ticketusers",
+                    localField: "_id",
+                    foreignField: "ticketID",
+                    as: "ticketUser"
+                }
+            },
+            { $unwind: "$ticketUser" },
+            {
+                $project: {
+                    closureTimeExists: { $gt: [{ $ifNull: ["$resolution.closureTime", null] }, null] },
+                    createdThisWeek: {
+                        $and: [
+                            { $eq: ["$ticketUser.interactionType", "create"] },
+                            { $gte: ["$ticketUser.interactionDate", startOfWeek] },
+                            { $lte: ["$ticketUser.interactionDate", now] }
+                        ]
+                    },
+                    resolvedThisWeek: {
+                        $and: [
+                            { $gte: ["$resolution.closureTime", startOfWeek] },
+                            { $lte: ["$resolution.closureTime", now] }
+                        ]
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    allActiveTickets: {
+                        $sum: {
+                            $cond: [
+                                { $eq: ["$closureTimeExists", false] },
+                                1,
+                                0
+                            ]
+                        }
+                    },
+                    newTicketsThisWeek: {
+                        $sum: { $cond: ["$createdThisWeek", 1, 0] }
+                    },
+                    completedThisWeek: {
+                        $sum: { $cond: ["$resolvedThisWeek", 1, 0] }
+                    }
+                }
+            }
+        ];
+
+        const result = await ticket.aggregate(aggregationPipeline).exec();
+        return result[0] || { allActiveTickets: 0, newTicketsThisWeek: 0, completedThisWeek: 0 };
+
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error calculating ticket stats');
+    }
+};
+
+
+
 //@ts-ignore
 export const getReport = async (_req, res) => {
     try {
         
         const ticketCounts = await calculateTicketCounts();
         const avgResolutionTime = await calculateAvgResolutionTime();
-        res.json({ ticketCounts, avgResolutionTime});
+        const mostReportedCategories = await calculateMostReportedCategories();
+        const ticketStats = await calculateTicketStats();
+
+        res.json({ ticketCounts, avgResolutionTime, mostReportedCategories, ticketStats});
     } catch (error) {
         console.error(error);
         res.status(500).send("Internal Server Error");
