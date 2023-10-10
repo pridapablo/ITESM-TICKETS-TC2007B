8ff2f44eb2a01bd7519e1ae3095b5c349c1779e6
Pablo Banzo <pridapablo@gmail.com>
2023-10-09 18:09:45 -0600

    initial report

--- api/src/controllers/report.controller.ts
+++ api/src/controllers/report.controller.ts
@@ -1,16 +1,40 @@
-// import user from "../models/user";
+import ticket from '../models/ticket';
 
 //@ts-ignore
-export const getReport = (_req, res) => {
-    res.send("Hola esto es una prueba");
-    // Usuario con más y menos tickets (aula con más y menos incidentes)
+export const getReport = async (_req, res) => {
+    try {
+        const aggregationPipeline = [
+            // This stage joins the TicketUser collection on the ticketID field
+            {
+                $lookup: {
+                    from: "ticketusers",
+                    localField: "_id",
+                    foreignField: "ticketID",
+                    as: "ticketUser"
+                }
+            },
+            // This stage unwinds the ticketUser array into individual documents
+            {
+                $unwind: "$ticketUser"
+            },
+            // Other stages to calculate the necessary report data...
+        ];
 
-    // Tiempo promedio en el que un ticket fue resuelto (merge de ticket.resolution.closureTime y ticketUser.ineractionDate when interactionType === "create")
+        const reportData = await ticket.aggregate(aggregationPipeline).exec();
 
-    // Categorías más reportadas (list all tickets in time range and count)
+        // Process reportData to calculate:
+        // 1. User with most and least tickets
+        // 2. Classroom with most and least incidents
+        // 3. Average time to resolve a ticket
+        // 4. Most reported categories
+        // 5. Number of active, new, and completed tickets
+        // 6. Inventory by classroom
 
-    // Numero de tickets activos (closure time == null), nuevos (closure time == null && ticketUser.ineractionDate when interactionType === "create" during the week), completados (closure time en la semana)
+        // Send the processed report data back to the frontend
+        res.json({ reportData });
 
-    // Inventario por aula (categoría mobiliario y materiales count de subcategorías)
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
 };
-
