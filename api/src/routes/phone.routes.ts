f2c66cb7da7f4692919f0d3e474815b1d55a84a5
Pablo Banzo <pridapablo@gmail.com>
2023-10-10 19:47:43 -0600

    added mw to phone, report and user

--- api/src/routes/phone.routes.ts
+++ api/src/routes/phone.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import {  handleTicket, phoneConfirmation } from "../controllers/phone.controllers";
+import { TokenValidation } from "../middlewares/authJwt";
 
 const router = Router();
 
 // Phone added to user
-router.post('/confirm', phoneConfirmation);
+router.post('/confirm', TokenValidation, phoneConfirmation); 
 
 // Chatbot
-router.post('/ticket', handleTicket);
+router.post('/ticket', handleTicket);// NO MIDDLEWARE (TWILIO PINGS)
     
 export default router;
