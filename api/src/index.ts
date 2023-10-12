37804449773e113324c7329e9aa6e2f59f148b7b
Miguel Angel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2023-10-11 18:16:34 -0600

    https server config

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,14 @@
 import app from './app';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+
 import './database';
 
-app.listen(8000, ()=>{
-    console.log(`The 8000 is listening`);
-})
+https.createServer({
+  cert: fs.readFileSync(path.join(__dirname, 'certs/backend.cer')), // Ruta al certificado (backend.cer)
+  key: fs.readFileSync(path.join(__dirname, 'certs/backend.key'))     // Ruta a la clave privada (backend.key)
+}, app).listen(8000, () => {
+  console.log("Servidor escuchando en puerto 8000"); // Corregido el mensaje
+    console.log(path.join(__dirname));
+});
